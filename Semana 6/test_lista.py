e154eabb29f0cbcc112ef0158395cb1fbdc2a403
JoseM810 <jose.miguel.alvarez.ortega@gmail.com>
2026-09-19 01:39:15 +0000

    nuevo archivo

---
+++ Semana 6/test_lista.py
@@ -0,0 +1,12 @@
+# Código base — Semana 06
+# Fuente: 01-Momento-1-Contrato-y-secuencia/06-Semana-06-Listas-enlazadas-simples/02-guia-de-laboratorio.html
+
+from lista_arreglo import ListaArreglo
+from lista_enlazada import ListaEnlazada
+
+# ANTES:  IMPLEMENTACIONES = [ListaArreglo]
+IMPLEMENTACIONES = [ListaArreglo, ListaEnlazada]
+
+# No cambies NADA más. Ejecuta:
+#     pytest -v
+# Deberías ver cada prueba corriendo dos veces, una por implementación.
